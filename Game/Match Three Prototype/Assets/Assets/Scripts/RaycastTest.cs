c8c1601c4d355d7ea15123ce37fa19875e88bec7
Shane Stephenson <s.stephenson94@outlook.com>
2016-11-02 09:27:47 +0000

    Match 3 prototype with touch being registered.

---
+++ Game/Match Three Prototype/Assets/Assets/Scripts/RaycastTest.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaycastTest : MonoBehaviour {
+
+	Vector3 touchPosWorld;
+
+	TouchPhase touchPhase = TouchPhase.Ended;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.touchCount > 0 && Input.GetTouch(0).phase == touchPhase)
+		{
+			touchPosWorld = Camera.main.ScreenToWorldPoint (Input.GetTouch (0).position);
+
+			Vector2 touchPosWorld2D = new Vector2 (touchPosWorld.x, touchPosWorld.y);
+
+			RaycastHit2D hitInformation = Physics2D.Raycast (touchPosWorld2D, Camera.main.transform.forward);
+
+			if (hitInformation.collider != null) {
+
+				GameObject touchedObject = hitInformation.transform.gameObject;
+
+				Debug.Log ("Touched " + touchedObject.transform.name);
+			}
+		}
+	}
+}
