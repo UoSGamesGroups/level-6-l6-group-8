7463ea7568a3a7cc58e9f76b33df2e28289d0bd5
Shane Stephenson <s.stephenson94@outlook.com>
2016-11-15 18:49:20 +0000

    no message

---
+++ Game/Updated Match Three/Match Three Prototype/Assets/Assets/Scripts/TouchControls.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchControls : MonoBehaviour {
+
+	void Update() {
+
+		int nbTouches = Input.touchCount;
+
+		if (nbTouches > 0) {
+			for (int i = 0; i < nbTouches; i++) {
+				{
+					Touch touch = Input.GetTouch (i);
+
+					TouchPhase phase = touch.phase;
+				}
+			}
+		}
+	}
+}
