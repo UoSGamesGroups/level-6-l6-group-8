a44ce66bb0ca71039439344b374a1fba5c87eeed
Thomas Smith <s168176@ucs.ac.uk>
2017-01-25 01:37:51 +0000

    Updated Game File and Vox files

---
+++ Game/Game/Assets/Code/Map/LevelLoad2.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class LevelLoad2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetButtonDown ("R")) 
+		{
+			SceneManager.LoadScene ("Words", LoadSceneMode.Single);
+		}
+	}
+}
