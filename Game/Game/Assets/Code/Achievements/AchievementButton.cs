12a132279b960fdec388c0aa32c65f7fd03797bb
Thomas Smith <s168176@ucs.ac.uk>
2017-02-18 17:14:44 +0000

    Achievment System

---
+++ Game/Game/Assets/Code/Achievements/AchievementButton.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AchievementButton : MonoBehaviour 
+{
+
+	public GameObject achievmentList;
+
+	public Sprite neutral, highlight;
+
+	private Image sprite;
+
+	void Awake()
+	{
+		sprite = GetComponent<Image> ();
+	
+	}
+
+	public void Click()
+	{
+		if (sprite.sprite == neutral) {
+			sprite.sprite = highlight;
+			achievmentList.SetActive (true);
+		} 
+		else 
+		{
+			sprite.sprite = neutral;
+			achievmentList.SetActive (false);
+		}
+	}
+}
